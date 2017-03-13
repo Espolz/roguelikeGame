0151193cbe3d241f86cccd7bb80ba7b7831cd694
Espolz <llegall16@gmail.com>
2017-03-13 21:56:45 +0100

    better project structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from 'react-redux';
-import App from './App';
 import './index.css';
 
 ReactDOM.render(
