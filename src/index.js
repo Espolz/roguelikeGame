80b0a6f6569b9977b193b3e697751e0b8e864ea5
Espolz <llegall16@gmail.com>
2017-03-13 21:25:21 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, combineReducers } from 'react-redux';
+import App from './App';
+import './index.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
